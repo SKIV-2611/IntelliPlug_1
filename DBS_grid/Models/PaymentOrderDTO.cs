2c3adaf3d4fed1425af3827002e672f313d64308
SKIV-2611 <didm.platonov@yandex.ru>
2021-08-04 18:57:55 +0300

    Added a conversion constructor for DTO

--- DBS_grid/Models/PaymentOrderDTO.cs
+++ DBS_grid/Models/PaymentOrderDTO.cs
@@ -1,23 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Text.Json.Serialization;
+//using System.Text.Json.Serialization;
 
 namespace DBS_grid.Models
 {
     public class PaymentOrderDTO
     {
-        [JsonPropertyName("DboID")]
-        public int ID { get; set; }
-        [JsonPropertyName("PayerAccountNumber")]
-        public string SenderAccount { get; set; }
-        [JsonPropertyName("ReceiverAccountNumber")]
-        public string ReceiverAccount { get; set; }
-        [JsonPropertyName("Amount")]
-        public decimal Sum { get; set; }
-        [JsonPropertyName("PaymentDetails")]
-        public string Reference { get; set; }
+        //[JsonPropertyName("DboID")]
+        //public int ID { get; set; }
+        public int DboID { get; set; }
+        //[JsonPropertyName("PayerAccountNumber")]
+        //public string SenderAccount { get; set; }
+        public string PayerAccountNumber { get; set; }
+        //[JsonPropertyName("ReceiverAccountNumber")]
+        //public string ReceiverAccount { get; set; }
+        public string ReceiverAccountNumber { get; set; }
+        //[JsonPropertyName("Amount")]
+        //public decimal Sum { get; set; }
+        public decimal Amount { get; set; }
+        //[JsonPropertyName("PaymentDetails")]
+        //public string Reference { get; set; }
+        public string PaymentDetails { get; set; }
 
+        public PaymentOrderDTO (PaymentOrder payment)
+        {
+            this.DboID = payment.ID;
+            this.PayerAccountNumber = payment.SenderAccount;
+            this.ReceiverAccountNumber = payment.ReceiverAccount;
+            this.Amount = payment.Sum;
+            this.PaymentDetails = payment.Reference;
+        }
     }
 }
