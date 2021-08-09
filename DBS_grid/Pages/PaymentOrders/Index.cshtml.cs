030e762e83bbdf19f4ba6a66bb06996f7d2f4a63
SKIV-2611 <didm.platonov@yandex.ru>
2021-08-10 00:30:06 +0200

    OnPostAsync to AltPaymentApi

--- DBS_grid/Pages/PaymentOrders/Index.cshtml.cs
+++ DBS_grid/Pages/PaymentOrders/Index.cshtml.cs
@@ -5,18 +5,24 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using RestEase;
+using Newtonsoft.Json;
 using DBS_grid.Data;
 using DBS_grid.Models;
+using DBS_grid.Interfaces;
 
 namespace DBS_grid.Pages.PaymentOrders
 {
     public class IndexModel : PageModel
     {
-        private readonly DBS_grid.Data.DBS_gridContext _context;
+        private readonly DBS_gridContext _context;
+        private readonly IPostPayment _postPayment;
 
-        public IndexModel(DBS_grid.Data.DBS_gridContext context)
+        public IndexModel(DBS_gridContext context)
         {
             _context = context;
+            _postPayment =
+                RestClient.For<IPostPayment>("http://localhost:54616");
         }
 
         public IList<PaymentOrder> PaymentOrder { get;set; }
@@ -24,6 +30,20 @@ namespace DBS_grid.Pages.PaymentOrders
         public async Task OnGetAsync()
         {
             PaymentOrder = await _context.PaymentOrder.ToListAsync();
+        }
+
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var paymentOrders =
+                _context.PaymentOrder.Where
+                (p => p.Status == Models.PaymentOrder.OrderStatus.Processing);
+            foreach(PaymentOrder po in paymentOrders)
+            {
+                await _postPayment.SendPayment(new PaymentOrderDTO(po));
+            }
+
+            return RedirectToPage();
         }
     }
 }
