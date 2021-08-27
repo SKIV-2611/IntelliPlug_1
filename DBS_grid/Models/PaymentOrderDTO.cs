c91e2b30f4492875960b8e31bb5ac5605280240b
SKIV-2611 <didm.platonov@yandex.ru>
2021-08-27 20:05:29 +0300

    Refactoring

--- DBS_grid/Models/PaymentOrderDTO.cs
+++ DBS_grid/Models/PaymentOrderDTO.cs
@@ -1,23 +1,16 @@
-﻿//using System.Text.Json.Serialization;
-
+﻿
 namespace DBS_grid.Models
 {
     public class PaymentOrderDTO
     {
-        //[JsonPropertyName("DboID")]
-        //public int ID { get; set; }
         public int DboID { get; set; }
-        //[JsonPropertyName("PayerAccountNumber")]
-        //public string SenderAccount { get; set; }
+
         public string PayerAccountNumber { get; set; }
-        //[JsonPropertyName("ReceiverAccountNumber")]
-        //public string ReceiverAccount { get; set; }
+
         public string ReceiverAccountNumber { get; set; }
-        //[JsonPropertyName("Amount")]
-        //public decimal Sum { get; set; }
+
         public decimal Amount { get; set; }
-        //[JsonPropertyName("PaymentDetails")]
-        //public string Reference { get; set; }
+
         public string PaymentDetails { get; set; }
 
         public PaymentOrderDTO(PaymentOrder payment)
